767ac6d6a59280d92260a9ca3485706c9f57f4e2
sergio <crgiosk.23@gmail.com>
2019-10-07 16:58:06 -0500

    Interfaces terminadas y validacion de inputs en compra

--- app/src/main/java/com/wiedii/wiicandy/vistas/FragmentCompra.kt
+++ app/src/main/java/com/wiedii/wiicandy/vistas/FragmentCompra.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.google.android.material.snackbar.Snackbar
 
 import com.wiedii.wiicandy.R
 import kotlinx.android.synthetic.main.fragment_compra.*
@@ -27,10 +28,47 @@ class FragmentCompra : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         buttonCompras.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_fragmentHome_to_fragment_compras2)
+           if (!validarTodosInputs()){
+               Navigation.findNavController(it).navigate(R.id.action_fragmentHome_to_fragment_compras2)
+           }else{
+               showError()
+           }
         }
     }
 
+
+
+
+
+
+
+
+    private fun validarTodosInputs(): Boolean {
+        return editTextProducto.text.isNullOrEmpty() || editTextCantidad.text.isNullOrEmpty() || editTextTotal.text.isNullOrEmpty()
+    }
+
+
+    fun showError() {
+        val required = "Informacion requerida"
+
+        if (editTextProducto.text.isNullOrEmpty()) {
+            editTextProducto.error = "No llevaste nada?"
+        }
+        if (editTextCantidad.text.isNullOrEmpty()) {
+            editTextCantidad.error = "Llevaste algo que no se puede contar?"
+        }
+        if (editTextTotal.text.isNullOrEmpty()) {
+            editTextTotal.error = "Te vas a ir sin pagar?"
+        }
+
+        if (editTextProducto.text.isNullOrEmpty() && editTextCantidad.text.isNullOrEmpty() && editTextTotal.text.isNullOrEmpty()){
+            editTextProducto.error = required
+            editTextCantidad.error = required
+            editTextTotal.error = required
+            Snackbar.make(activity!!.findViewById(android.R.id.content),"Informacion requerida",Snackbar.LENGTH_LONG).show()
+        }
+
+    }
 
 
 }
