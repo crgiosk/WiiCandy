1bf10d6e2b5401a04525063afd8b19645c1aac5e
sergio <crgiosk.23@gmail.com>
2019-10-14 16:56:09 -0500

    Formulario terminado -> implementar scroll view si se puede

--- app/src/main/java/com/wiedii/wiicandy/UI/vistas/FragmentRegistro.kt
+++ app/src/main/java/com/wiedii/wiicandy/UI/vistas/FragmentRegistro.kt
@@ -2,22 +2,25 @@ package com.wiedii.wiicandy.UI.vistas
 
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.navigation.Navigation
-
+import android.widget.*
 import com.wiedii.wiicandy.R
-import kotlinx.android.synthetic.main.fragment_fragment_login.*
 import kotlinx.android.synthetic.main.fragment_registro.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class FragmentRegistro : Fragment() {
+    private val spinner_Sexo_opciones = arrayOf("Seleccione...", "Hombre", "Mujer")
+    private lateinit var nombres: EditText
+    private lateinit var apellidos: EditText
+    private lateinit var correo: EditText
+    private lateinit var celular: EditText
+    private lateinit var sexo: Spinner
 
 
     override fun onCreateView(
@@ -31,12 +34,88 @@ class FragmentRegistro : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setSpinner()
+        setValues()
+
+        buttonRegistrar.setOnClickListener {
+            alerts()
+
+            if (!emptyFields()){
+                Toast.makeText(context,"A guardar",Toast.LENGTH_LONG).show()
+            }else{
+                alerts()
+            }
+        }
     }
 
-    private fun setSpinner(){
-        val spinner_Sexo = arrayOf("Seleccione...","Hombre","Mujer")
-         val adapter = ArrayAdapter<String>(activity!!.applicationContext,R.layout.spinner_item_style,spinner_Sexo)
+    private fun setSpinner() {
+        val adapter = ArrayAdapter<String>(
+            activity!!.applicationContext,
+            R.layout.spinner_item_style,
+            spinner_Sexo_opciones
+        )
         spinnerSexo.adapter = adapter
+    }
+
+
+    private fun alerts() {
+        val required = "Requerido"
+        if (nombres.text.isNullOrEmpty()) {
+            textInputLayoutNombres.setError(required)
+        }else{
+            textInputLayoutNombres.setError(null)
+        }
+
+        if (apellidos.text.isNullOrEmpty()) {
+            textInputLayoutApellidos.setError(required)
+        }else{
+            textInputLayoutApellidos.setError(null)
+        }
+
+        if (editTextFechaNacimiento.text.isNullOrEmpty()) {
+            TextInputLayoutFechaNacimiento.setError(required)
+        }else{
+            TextInputLayoutFechaNacimiento.setError(null)
+        }
+
+        if (sexo.selectedItem.toString() == spinner_Sexo_opciones[0]) {
+            TextInputLayoutSexo.setError(required)
+        }else{
+            TextInputLayoutSexo.setError(null)
+        }
+
+        if (correo.text.isNullOrEmpty()) {
+            TextInputLAyoutEmail.setError(required)
+        }else if (!validarCorreo(correo.text.toString())){
+            TextInputLAyoutEmail.setError("Correo no valido")
+        }else{
+            TextInputLAyoutEmail.setError(null)
+        }
+
+        if (celular.text.isNullOrEmpty()) {
+            TextInputLayoutCelular.setError(required)
+        }else{
+            TextInputLayoutCelular.setError(null)
+        }
+    }
+
+    private fun emptyFields() = nombres.text.isNullOrEmpty()
+            || apellidos.text.isNullOrEmpty()
+            || celular.text.isNullOrEmpty()
+            || correo.text.isNullOrEmpty()
+            || celular.text.isNullOrEmpty()
+            || sexo.selectedItem.toString() == spinner_Sexo_opciones[0]
+
+    private fun setValues() {
+        nombres = editTextRegistroNombres
+        apellidos = editTextRegistroApellidos
+        sexo = spinnerSexo
+        celular = editTextRegistroCelular
+        correo = editTextRegistroEmail
+    }
+
+    fun validarCorreo(corre:String):Boolean{
+        val pattern = Patterns.EMAIL_ADDRESS
+        return pattern.matcher(corre).matches()
     }
 
 
